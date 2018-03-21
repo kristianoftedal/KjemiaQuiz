d4fb467ce5810d676fef5f3a16d6880a4c101d82
kristianoftedal <kristian.oftedal@pointtaken.no>
2018-03-21 18:59:32 +0100

    adjusted image size

--- src/containers/App/index.style.js
+++ src/containers/App/index.style.js
@@ -1,11 +1,12 @@
 /* @flow */
 import { StyleSheet } from 'react-native';
+import metrics from '../../config/metrics';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    width: undefined,
-    height: undefined,
+    width: metrics.DEVICE_WIDTH,
+    height: metrics.DEVICE_HEIGHT,
     flexDirection: 'column',
     backgroundColor: 'transparent',
     justifyContent: 'center',

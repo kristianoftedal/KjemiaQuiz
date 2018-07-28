e0592b08f84f035d3b24835b0d98d4aeb6348e8f
kristianoftedal <kristian.oftedal@pointtaken.no>
2018-07-28 13:44:47 +0200

    upgrade

--- ios/KjemiaQuiz/AppDelegate.m
+++ ios/KjemiaQuiz/AppDelegate.m
@@ -1,29 +1,25 @@
 /**
  * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
+
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-#import "SplashScreen.h"
-@import GoogleMobileAds;
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"KjemiaQuiz"
+                                                      moduleName:@"kjemiaquiz"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
@@ -31,11 +27,8 @@
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
-  [GADMobileAds configureWithApplicationID:@"ca-app-pub-4545695212875309~6741517180"];
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  
-  [SplashScreen show];
   return YES;
 }
 

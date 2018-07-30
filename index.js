e1194bbb2e856e485960b8014e345f5e14d6d99b
kristianoftedal <kristian.oftedal@pointtaken.no>
2018-07-30 18:04:02 +0200

    upgrade

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import KjemiaQuiz from './src';
 
-AppRegistry.registerComponent('KjemiaQuiz', () => KjemiaQuiz);
+AppRegistry.registerComponent('kjemiaquiz', () => KjemiaQuiz);
